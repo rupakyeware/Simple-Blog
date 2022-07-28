a7c64ffa2e992e6bf902feb96e4930de3b0f9208
rupakyeware <rupakyeware@gmail.com>
2022-07-28 13:49:49 +0530

    Solved challenge 16 bug

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const { zipWith, forEach } = require("lodash");
 
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -18,7 +19,7 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-const posts=[]
+const posts=[{title: "This is the title", post: "This is the body"}]
 
 app.get("/", (req, res) => {
   let options = {
@@ -41,15 +42,18 @@ app.get("/about", (req, res) => {
 
 app.get('/posts/:title', (req, res) =>{
   let title = req.params.title;
-  let post = posts.find(element => element.title === title);
-  let options = {
-    title: title,
-    post: post
-  }
 
-  console.log(req.params.title)
-  console.log(post);
-  res.render("post", options);
+  //works for now but needs to be optimised
+  forEach(posts, (post) => {
+    if (post.title === title) {
+      let options = {
+        title : title,
+        post: post.post
+      }
+
+      res.render("post", options);
+    }
+  })
 })
 
 app.get("/contact", (req, res) => {
